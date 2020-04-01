7d54afb31dda4fbf68fde4b570b2ad205cf6d815
ваше имяwlad999 <wladmail25@gmeil.com>
2020-04-01 20:43:16 +0300

    Updates

---
+++ precache-manifest.6d4e6b6b7919eb755588afc2124704aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf0783d7f10874be72be6b4c5256146",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "5be1c5251c27f7a4b2e8",
+    "url": "/Weather/static/css/main.743fb3fc.chunk.css"
+  },
+  {
+    "revision": "4fdc43e90be66ede66af",
+    "url": "/Weather/static/js/2.b4bc6af3.chunk.js"
+  },
+  {
+    "revision": "44a16c06f03038c638c260daad7519b0",
+    "url": "/Weather/static/js/2.b4bc6af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5be1c5251c27f7a4b2e8",
+    "url": "/Weather/static/js/main.a48eed5e.chunk.js"
+  },
+  {
+    "revision": "ac623d8251042772f154",
+    "url": "/Weather/static/js/runtime-main.da410601.js"
+  },
+  {
+    "revision": "ff94c5ecb03b5eea899a158444d5850f",
+    "url": "/Weather/static/media/point.ff94c5ec.svg"
+  }
+]);
