ab60a6f1862d41384269873753d6b49952987894
wlad999 <wladmail25@gmeil.com>
2020-04-03 18:50:42 +0300

    changed GoogleMap from class to pure function

--- src/components/GoogleMap/GoogleMap.js
+++ src/components/GoogleMap/GoogleMap.js
@@ -2,31 +2,23 @@ import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
 import Marker from "../Marker/Marker";
 
-class GoogleMaps extends Component {
-  static defaultProps = {
-    center: {
-      lat: 52.374,
-      lng: 4.8897,
-    },
-    zoom: 11,
-  };
+const GoogleMaps = ({ showCurrenWeather, center }) => {
+  const { lat, lng } = center;
 
-  render() {
-    const { lat, lng } = this.props.center;
-    const { showCurrenWeather } = this.props;
-
-    return (
-      <div style={{ height: "430px", width: "100%" }}>
-        <GoogleMapReact
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-          center={this.props.center}
-        >
-          <Marker lat={lat} lng={lng} showCurrenWeather={showCurrenWeather} />
-        </GoogleMapReact>
-      </div>
-    );
-  }
-}
+  return (
+    <div style={{ height: "430px", width: "100%" }}>
+      <GoogleMapReact
+        defaultCenter={{
+          lat: 52.374,
+          lng: 4.8897,
+        }}
+        defaultZoom={11}
+        center={center}
+      >
+        <Marker lat={lat} lng={lng} showCurrenWeather={showCurrenWeather} />
+      </GoogleMapReact>
+    </div>
+  );
+};
 
 export default GoogleMaps;
