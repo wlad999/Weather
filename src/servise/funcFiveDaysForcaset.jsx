3a840ae33087e2548434ab3793c94b8dbb63e31f
wlad999 <wladmail25@gmeil.com>
2020-04-03 15:48:12 +0300

    funcFiveDaysForcaset  cleaning

--- src/servise/funcFiveDaysForcaset.jsx
+++ src/servise/funcFiveDaysForcaset.jsx
@@ -8,32 +8,28 @@ const funcFiveDaysForcaset = props => {
   let forcasetDayList = {};
   let dayForecast = null;
 
-  try {
-    if (fiveDayOwnWeather.list) {
-      forcasetDayList = fiveDayOwnWeather.list.filter(item => {
-        const dayin = moment(item.dt * 1000).format("MMMM, D");
-        if (rightDay === dayin) {
-          return item;
-        }
-      });
-      dayForecast = forcasetDayList.map(item => {
-        const time = moment(item.dt * 1000).format("HH:mm");
+  if (fiveDayOwnWeather.list) {
+    forcasetDayList = fiveDayOwnWeather.list.filter(item => {
+      const dayin = moment(item.dt * 1000).format("MMMM, D");
+      if (rightDay === dayin) {
+        return item;
+      }
+    });
+    dayForecast = forcasetDayList.map(item => {
+      const time = moment(item.dt * 1000).format("HH:mm");
 
-        return (
-          <div key={item.dt} className="small">
-            <span key={item.dt}>
-              {time}
-              <span className="pl-2">
-                {item.main.temp}&deg;C, {item.weather[0].main},
-              </span>
+      return (
+        <div key={item.dt} className="small">
+          <span key={item.dt}>
+            {time}
+            <span className="pl-2">
+              {item.main.temp}&deg;C, {item.weather[0].main},
             </span>
-            <p>Wind - {item.wind.speed} meter/sec</p>
-          </div>
-        );
-      });
-    }
-  } catch (error) {
-    console.log(error);
+          </span>
+          <p>Wind - {item.wind.speed} meter/sec</p>
+        </div>
+      );
+    });
   }
 
   return dayForecast;
